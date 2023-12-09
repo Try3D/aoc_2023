150a4def0a452203d288dbbe966c4c992453a7b7
try3d <leptonaray@gmail.com>
2023-12-09 20:13:49 +0530

    Add day 9 solution

---
+++ day_09/part_1.py
@@ -0,0 +1,20 @@
+with open("input.txt") as file:
+    input = [line.rstrip().split() for line in file]
+
+input = [[int(i) for i in line] for line in input]
+
+
+def find_next(seq):
+    if not len(seq):
+        return 0
+
+    pred = [seq[i + 1] - seq[i] for i in range(len(seq) - 1)]
+
+    n = find_next(pred)
+
+    return seq[-1] + n
+
+
+ans = sum([find_next(line) for line in input])
+
+print(ans)
---
+++ day_09/part_2.py
@@ -0,0 +1,19 @@
+with open("input.txt") as file:
+    input = [line.rstrip().split() for line in file]
+
+input = [[int(i) for i in line] for line in input]
+
+
+def find_prev(seq):
+    if not len(seq):
+        return 0
+
+    pred = [seq[i + 1] - seq[i] for i in range(len(seq) - 1)]
+
+    n = find_prev(pred)
+
+    return seq[0] - n
+
+ans = sum([find_prev(line) for line in input])
+
+print(ans)
